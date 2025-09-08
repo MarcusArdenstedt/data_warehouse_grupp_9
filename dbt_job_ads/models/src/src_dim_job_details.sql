2e1a149c8e84d48f8934b8b01c4951a339093f87
Marcus <marcus.ericsson_92@icloud.com>
2025-09-08 17:42:19 +0200

    setup sources folder with src sql files

---
+++ dbt_job_ads/models/src/src_dim_job_details.sql
@@ -0,0 +1,14 @@
+
+WITH job_details AS (SELECT * FROM {{ source('job_ads', 'stg_job_ads') }})
+
+SELECT
+    headline,
+    description__text AS "description", 
+    description__text_formatted AS description_formatted,
+    employment_type__label AS employment_typ,
+    duration__label AS duration,
+    salary_type__label AS salary_type,
+    scope_of_work__min AS max_scope,
+    scope_of_work__max AS min_scope,
+FROM 
+    job_details
--- dbt_job_ads/models/src/src_fct_job_ads.sql
+++ dbt_job_ads/models/src/src_fct_job_ads.sql
@@ -1,5 +1,9 @@
 WITH job_ads AS (
     SELECT * FROM {{ source('job_ads', 'stg_job_ads') }}
     )
-SELECT *
+SELECT 
+    number_of_vacancies,
+    application_deadline,
+    publication_date,
+    last_publication_date
 FROM job_ads
