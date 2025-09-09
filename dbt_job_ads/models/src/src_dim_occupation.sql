f19137b45bc689d750b033f9fc4d1d57cf1f741f
John Sandsjö <john.sandsjo@gmail.com>
2025-09-09 18:06:23 +0200

    dbt transformations

--- dbt_job_ads/models/src/src_dim_occupation.sql
+++ dbt_job_ads/models/src/src_dim_occupation.sql
@@ -2,10 +2,9 @@
 WITH src_occupation AS (SELECT * FROM {{ source('job_ads', 'stg_job_ads') }})
 
 SELECT
+    id,
     OCCUPATION__LABEL AS occupation,
     OCCUPATION_FIELD__LABEL AS occupation_field,
     OCCUPATION_GROUP__LABEL AS occupation_group
 FROM 
     src_occupation
-
-
