8ff71aaf52f4ab5ef29012a1d2135ea97d3d9ca2
Marcus <marcus.ericsson_92@icloud.com>
2025-09-12 12:42:45 +0200

    create role file to each role

--- worksheets/setup_role_transformer.sql
+++ worksheets/setup_role_transformer.sql
@@ -1,7 +1,6 @@
 
 USE ROLE USERADMIN;
 
-CREATE ROLE IF NOT EXISTS job_loader;
 
 CREATE ROLE IF NOT EXISTS job_transformer;
 
@@ -10,16 +9,6 @@ CREATE ROLE IF NOT EXISTS job_transformer;
 
 USE ROLE SECURITYADMIN;
 
---dlt user
-GRANT ROLE job_loader TO USER dlt_user;
-GRANT USAGE ON DATABASE job_db TO ROLE job_loader;
-
-GRANT USAGE ON SCHEMA job_db.staging TO ROLE job_loader;
-
-GRANT CREATE TABLE ON SCHEMA job_db.staging TO ROLE job_loader;
-
-
-SHOW ROLES;
 
 --dbt user
 
--- worksheets/setup_users.sql
+++ worksheets/setup_users.sql
@@ -1,6 +1,7 @@
 
-USE ROLE USERADMIN;
 
+-- create user dlt_user
+USE ROLE USERADMIN;
 
 CREATE USER IF NOT EXISTS dlt_user
     PASSWORD = "dlt_user123"
@@ -12,6 +13,7 @@ CREATE USER IF NOT EXISTS dlt_user
 
 SELECT current_role();
 
+-- create user dbt_transformer
 USE ROLE USERADMIN;
 
 DROP USER dbt_user;
@@ -22,4 +24,15 @@ CREATE USER IF NOT EXISTS dbt_user
     LOGIN_NAME = "dbt_user"
     DEFAULT_ROLE = "job_transformer"
     DEFAULT_NAMESPACE = "job_db.warehouse"
+    MUST_CHANGE_PASSWORD = TRUE;
+
+-- create user streamlit_user
+USE ROLE USERADMIN;
+
+CREATE USER IF NOT EXISTS streamlit_user
+    PASSWORD = "Streamlituser"
+    DEFAULT_WAREHOUSE = job_api_group_9
+    LOGIN_NAME = "streamlit_user"
+    DEFAULT_ROLE = "job_presenter"
+    DEFAULT_NAMESPACE = "job_db.marts"
     MUST_CHANGE_PASSWORD = TRUE;
